b962dea77569bc65a238ea9f095c4d53a1819910
SINJE2 <SINJE2@OOCL.COM>
2025-09-10 17:13:05 +0800

    feat: throw exception when employee create age is below 18

---
+++ src/test/java/org/example/springbootdemo/service/EmployeeServiceTest.java
@@ -0,0 +1,37 @@
+package org.example.springbootdemo.service;
+
+import org.example.springbootdemo.entity.Employee;
+import org.example.springbootdemo.exception.EmployeeAgeSalaryException;
+import org.example.springbootdemo.exception.EmployeeNotInAgeRangeException;
+import org.example.springbootdemo.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class EmployeeServiceTest {
+    @InjectMocks
+    EmployeeService employeeService;
+
+    @Mock
+    EmployeeRepository employeeRepository;
+
+    @Test
+    void should_throw_exception_when_create_employee_given_age_below_18(){
+        assertThrows(EmployeeNotInAgeRangeException.class, () -> {
+            employeeService.createEmployee(new Employee(1, "Mary", 17, 5000, "Female"));
+        });
+        verify(employeeRepository, never()).addEmployee(any());
+    }
+
+    
+}
